cc99e12ef8dae6cafa93978c10401e16dfc849a1
Zeyad Daowd <zeyaddaowd@yahoo.com>
2025-04-19 18:06:34 +0200

    Linear Model Tree Classes
    Tessellated Linear Model won't be viable for classification

--- Linear-Model-Tree.py
+++ Linear-Model-Tree.py
@@ -27,16 +27,17 @@ def gain_ratio(y, y_left, y_right):
     if split_entro == 0:
         return 0
     return gain / split_entro
-class TLMNode:
+class LMTNode:
     def __init__(self):
-        self.result = None # for leaf nodes
+        self.result = None # for leaf nodes if pure
         self.left = None
         self.right = None
         self.split_rule = None
         self.model = None
+        self.switched_split = False ## if split model performs better than base model take it
         self.is_leaf = False
         self.split_value = None
-class TessellatedLinearModel(BaseEstimator, ClassifierMixin):
+class LinearModelTree(BaseEstimator, ClassifierMixin):
     def __init__(self, base_model, split_policy = 'Gain', split_model = None, max_depth = 5, min_samples_leaf = 5, min_samples_split = 10):
         self.base_model = base_model
         if split_policy not in ['Gain', 'Accuracy']:
@@ -52,24 +53,46 @@ class TessellatedLinearModel(BaseEstimator, ClassifierMixin):
         self.min_samples_split = min_samples_split
     def _accuracy_gain(self, X, y, left_mask, right_mask):
         if left_mask.sum() == 0 or right_mask.sum() == 0:
-            return -np.inf  # invalid split
+            return -np.inf, None, None  # invalid split
         accuracy_parent = self.split_model.score(X, y)
         if (accuracy_parent == 1.0):
-            return 0
+            return 0, None, None
         if (len(np.unique(y[left_mask])) == 1):
             accuracy_left = 1.0
+            model_left = None
         else:
             model_left = clone(self.split_model).fit(X[left_mask], y[left_mask])
             accuracy_left = model_left.score(X[left_mask], y[left_mask])
         if (len(np.unique(y[right_mask])) == 1):
             accuracy_right = 1.0
+            model_right = None
         else:
             model_right = clone(self.split_model).fit(X[right_mask], y[right_mask])
             accuracy_right = model_right.score(X[right_mask], y[right_mask])
         accuracy_gain = accuracy_left * left_mask.sum() + accuracy_right * right_mask.sum() - accuracy_parent * len(X)
-        return accuracy_gain
-    def _fit_node(self, X, y, depth):
-        node = TLMNode()
+        return accuracy_gain, model_left, model_right
+    def _switch_model_leaf(self, X, y, node):
+        if (self.split_model is not None):
+            temp_split_model = clone(self.split_model).fit(X, y)
+            accuracy = temp_split_model.score(X, y)
+            if accuracy > node.model.score(X, y):
+                node.switched_split = True
+                node.model = temp_split_model
+            else:
+                node.switched_split = False
+    def _get_thresholds(self, X, feature_index):
+        feature_data = X[:, feature_index]
+        unique_values = np.unique(feature_data)
+        return unique_values
+        if len(unique_values) <= 100:
+            thresholds = unique_values
+        else:
+            percentiles = np.linspace(1, 100, 100)
+            thresholds = np.percentile(feature_data, percentiles)
+            thresholds = np.unique(thresholds)
+        return thresholds
+    def _fit_node(self, X, y, depth, model=None):
+        node = LMTNode()
 
         if (depth >= self.max_depth or
                 len(np.unique(y)) == 1 or
@@ -80,12 +103,16 @@ class TessellatedLinearModel(BaseEstimator, ClassifierMixin):
                 node.result = y[0]
                 return node
             node.model = clone(self.base_model).fit(X, y)
+            self._switch_model_leaf(X, y, node)
             return node
         best_gain = -np.inf
         best_split = None
         n_features = X.shape[1]
         if(self.split_policy == 'Accuracy'):
-            self.split_model.fit(X, y)
+            if(model is not None):
+                self.split_model = model
+            else:
+                self.split_model.fit(X, y)
             accuracy = self.split_model.score(X, y)
             if accuracy == 1.0:
                 node.is_leaf = True
@@ -93,7 +120,7 @@ class TessellatedLinearModel(BaseEstimator, ClassifierMixin):
                 node.result = y[0]
                 return node
         for feature_index in range(n_features):
-            thresholds = np.unique(X[:, feature_index])
+            thresholds = self._get_thresholds(X, feature_index)
             for threshold in thresholds:
                 left_mask = X[:, feature_index] <= threshold
                 right_mask = ~left_mask
@@ -106,12 +133,16 @@ class TessellatedLinearModel(BaseEstimator, ClassifierMixin):
 
                 if self.split_policy == 'Gain':
                     gain = gain_ratio(y, y_left, y_right)
+                    model_left = None
+                    model_right = None
                 else:
-                    gain = self._accuracy_gain(X, y, left_mask, right_mask)
+                    gain, model_left, model_right = self._accuracy_gain(X, y, left_mask, right_mask)
 
                 if gain > best_gain:
                     best_gain = gain
                     best_split = {
+                        'model_left': model_left,
+                        'model_right': model_right,
                         'feature_index': feature_index,
                         'threshold': threshold,
                         'left_mask': left_mask,
@@ -130,8 +161,8 @@ class TessellatedLinearModel(BaseEstimator, ClassifierMixin):
         X_left, y_left = X[best_split['left_mask']], y[best_split['left_mask']]
         X_right, y_right = X[best_split['right_mask']], y[best_split['right_mask']]
 
-        node.left = self._fit_node(X_left, y_left, depth + 1)
-        node.right = self._fit_node(X_right, y_right, depth + 1)
+        node.left = self._fit_node(X_left, y_left, depth + 1, model=best_split['model_left'])
+        node.right = self._fit_node(X_right, y_right, depth + 1, model=best_split['model_right'])
 
         return node
     def fit(self, X, y):
@@ -152,17 +183,39 @@ class TessellatedLinearModel(BaseEstimator, ClassifierMixin):
         predictions = self.predict(X)
         return np.mean(predictions == y)
 
+def try_class():
+    from sklearn.datasets import make_classification
+    from sklearn.model_selection import train_test_split
+    from sklearn.linear_model import LogisticRegression, SGDClassifier
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.tree import DecisionTreeClassifier
+    from time import time
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.svm import SVC as SVM
+    X, y = make_classification(n_samples=2000, n_features=27, n_informative=20,
+                               n_classes=7, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y)
 
-
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression, SGDClassifier
-X, y = make_classification(n_samples=200, n_features=5, n_informative=3,
-                           n_classes=3, random_state=42)
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y)
-
-model = TessellatedLinearModel(base_model=SGDClassifier(), split_policy = 'Accuracy', split_model=LogisticRegression(), max_depth=3, min_samples_leaf=5, min_samples_split=10)
-model.fit(X_train, y_train)
-print("Accuracy:", model.score(X_test, y_test))
-
-
+    model = LinearModelTree(base_model=LogisticRegression(max_iter=300), split_policy = 'Accuracy', split_model=KNeighborsClassifier(n_neighbors=2), max_depth=5, min_samples_leaf=5, min_samples_split=10)
+    curr_time = time()
+    model.fit(X_train, y_train)
+    print ("TLM RESULTS")
+    print("Training acc:", model.score(X_train, y_train))
+    print("Accuracy:", model.score(X_test, y_test))
+    print("Time taken:", time() - curr_time)
+    curr_time = time()
+    ## ----- -TREEE RESULTS ----- ###
+    print ("Tree RESULTS")
+    model = DecisionTreeClassifier(max_depth=3, min_samples_leaf=5, min_samples_split=10)
+    model.fit(X_train, y_train)
+    print("Training acc:", model.score(X_train, y_train))
+    print("Accuracy:", model.score(X_test, y_test))
+    ## ------ Random Forest ------ ####
+    print ("Random Forest RESULTS")
+    from sklearn.ensemble import RandomForestClassifier
+    model = RandomForestClassifier(max_depth=5, min_samples_leaf=5, min_samples_split=10)
+    model.fit(X_train, y_train)
+    print("Training acc:", model.score(X_train, y_train))
+    print("Accuracy:", model.score(X_test, y_test))
+    print("Total Time for the rest " + str(time() - curr_time))
+try_class()
